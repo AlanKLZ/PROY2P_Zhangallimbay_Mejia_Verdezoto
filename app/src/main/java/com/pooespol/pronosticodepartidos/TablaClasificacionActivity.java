1586abda5e152ceae45b2db5e441c6ac5164eeb7
Alan_KLZ <alanzhangallimbay23@gmail.com>
2026-08-15 01:21:13 -0500

    Codigo inicial del activity de los views iniciales

---
+++ app/src/main/java/com/pooespol/pronosticodepartidos/TablaClasificacionActivity.java
@@ -0,0 +1,42 @@
+package com.pooespol.pronosticodepartidos;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class TablaClasificacionActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ImageButton btnMenu;
+    private Button btnVolver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tabla_clasificacion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        drawerLayout = findViewById(R.id.drawerLayout);
+        btnMenu = findViewById(R.id.btnMenu);
+        btnVolver = findViewById(R.id.btnVolver);
+
+        //Abre el menu a la izquierda
+        btnMenu.setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+        //Termina la activity actual y regresa al menu principal
+        btnVolver.setOnClickListener(v->{
+            finish();
+        });
+    }
+}
