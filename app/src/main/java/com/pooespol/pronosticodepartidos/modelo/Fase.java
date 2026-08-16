4ae48f00182b6406e046dd3d2c51aff471448740
Naomi Mejía <lismejia@espol.edu.ec>
2026-08-16 10:14:31 -0500

    Agregar enum Fase según diagrama de clases

---
+++ app/src/main/java/com/pooespol/pronosticodepartidos/modelo/Fase.java
@@ -0,0 +1,11 @@
+package com.pooespol.pronosticodepartidos.modelo;
+
+public enum Fase {
+    GRUPOS,
+    DIECISEISAVOS,
+    OCTAVOS,
+    CUARTOS,
+    SEMIFINAL,
+    TERCER_PUESTO,
+    FINAL
+}
