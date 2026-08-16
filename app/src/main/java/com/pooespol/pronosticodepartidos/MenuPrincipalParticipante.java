70883fd03a2a7a4b39cd900bf959cd77a38d92d1
AndreaVerdezoto <andreaverdezotolung@gmail.com>
2026-08-16 01:59:07 -0500

    Escritura del código de MenuPrincipalParticipante

--- app/src/main/java/com/pooespol/pronosticodepartidos/MenuPrincipalParticipante.java
+++ app/src/main/java/com/pooespol/pronosticodepartidos/MenuPrincipalParticipante.java
@@ -1,6 +1,8 @@
 package com.pooespol.pronosticodepartidos;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuPrincipalParticipante extends AppCompatActivity {
+    private TextView tVnombreParticipante;
+    private Button btnTablaPosiciones;
+    private Button btnPronosticos;
+    private Button btnMisPronosticos;
+    private Button btnSalirParticipante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_principal_participante);
+        tVnombreParticipante = findViewById(R.id.tvnombreParticipante);
+        btnTablaPosiciones = findViewById(R.id.btnTablaPosiciones);
+        btnPronosticos = findViewById(R.id.btnPronósticos);
+        btnMisPronosticos = findViewById(R.id.btnMisPronosticos);
+        btnSalirParticipante = findViewById(R.id.btnSalirParticipante);
+        String nombreParticipante = getIntent().getStringExtra("nombreCompleto");
+        tVnombreParticipante.setText(nombreCompleto);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
