5960ad6c9f509043b78c00f8ded17dcc97bab879
Naomi Mejía <lismejia@espol.edu.ec>
2026-08-16 11:05:21 -0500

    Comentarioss

--- app/src/main/java/com/pooespol/pronosticodepartidos/modelo/EstadoPartido.java
+++ app/src/main/java/com/pooespol/pronosticodepartidos/modelo/EstadoPartido.java
@@ -1,5 +1,14 @@
 package com.pooespol.pronosticodepartidos.modelo;
 
+/**
+ * Enum que representa los posibles estados de un partido.
+ *
+ * ABIERTO: los participantes pueden registrar o modificar pronósticos.
+ * CERRADO: ya no se pueden modificar pronósticos.
+ * FINALIZADO: el partido terminó y ya puede tener un resultado oficial.
+ *
+ * @author Naomi
+ */
 public enum EstadoPartido {
     ABIERTO,
     CERRADO,
