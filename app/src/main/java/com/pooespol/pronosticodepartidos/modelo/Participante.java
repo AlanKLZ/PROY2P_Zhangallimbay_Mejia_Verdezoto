8b1aab6cc6e677373c045091abf8ae6230e6ec88
Naomi Mejía <lismejia@espol.edu.ec>
2026-08-16 11:19:07 -0500

    Agregar método registrarPronostico en Participante

--- app/src/main/java/com/pooespol/pronosticodepartidos/modelo/Participante.java
+++ app/src/main/java/com/pooespol/pronosticodepartidos/modelo/Participante.java
@@ -23,9 +23,58 @@ public class Participante extends Usuario implements Comparable<Participante>{
     public int getPuntajeAcumulado() {
         return puntajeAcumulado;
     }
+
     //setter
     public void setPuntajeAcumulado(int puntajeAcumulado) {
         this.puntajeAcumulado = puntajeAcumulado;
+    }
+
+    /**
+     * Registra un pronóstico para un partido.
+     *
+     * @param partido partido sobre el cual se realizará el pronóstico
+     * @param golesSeleccion1 goles pronosticados para la selección 1
+     * @param golesSeleccion2 goles pronosticados para la selección 2
+     * @return pronóstico registrado
+     * @throws PronosticoFueraDeTiempoException si el partido no está abierto
+     * @throws DatosIncompletosException si los datos ingresados no son válidos
+     * @author Naomi
+     */
+    public Pronostico registrarPronostico(
+            Partido partido,
+            int golesSeleccion1,
+            int golesSeleccion2)
+            throws PronosticoFueraDeTiempoException, DatosIncompletosException {
+
+        if (partido == null) {
+            throw new DatosIncompletosException(
+                    "No se han ingresado todos los datos necesarios para registrar el pronóstico."
+            );
+        }
+
+        if (golesSeleccion1 < 0 || golesSeleccion2 < 0) {
+            throw new DatosIncompletosException(
+                    "Los goles deben ser números enteros mayores o iguales a cero."
+            );
+        }
+
+        if (partido.getEstadoPartido() != EstadoPartido.ABIERTO) {
+            throw new PronosticoFueraDeTiempoException(
+                    "El período para registrar pronósticos de este partido ya ha finalizado."
+            );
+        }
+
+        String idPronostico =
+                this.getIdUsuario() + "_" + partido.getIdPartido();
+
+        return new Pronostico(
+                idPronostico,
+                this.getIdUsuario(),
+                partido.getIdPartido(),
+                golesSeleccion1,
+                golesSeleccion2,
+                0
+        );
     }
 
     /**
