7348b80b5d5fc7f60be886aadf31000b2188d3e5
Naomi Mejía <lismejia@espol.edu.ec>
2026-08-29 21:31:26 -0500

    Agregar toggle del menu lateral en PronosticosActivity

--- app/src/main/java/com/pooespol/pronosticodepartidos/PronosticosActivity.java
+++ app/src/main/java/com/pooespol/pronosticodepartidos/PronosticosActivity.java
@@ -10,11 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
+
 
 public class PronosticosActivity extends AppCompatActivity {
     private Spinner spFase;
     private LinearLayout llPartidos;
     private Button btVolver;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
 
     @Override
@@ -22,7 +32,7 @@ public class PronosticosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pronosticos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -31,6 +41,49 @@ public class PronosticosActivity extends AppCompatActivity {
         llPartidos= findViewById(R.id.llPartidos);
         btVolver = findViewById(R.id.btVolver);
 
+    // Configuracion del menu lateral
+    drawerLayout = findViewById(R.id.drawerLayout);
+    navigationView = findViewById(R.id.navigationView);
+    toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+        
+    // Abre el menu lateral con el boton de la barra superior
+    ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(
+        this,
+        drawerLayout,
+        toolbar,
+        R.string.abrir_menu,
+        R.string.cerar_menu
+     );
 
+        
+    drawerLayout.addDrawerListener(toogle);
+    toogle.syncState();
+
+    // Configuracion de los items del menu
+    navigationView.setNavigationItemSelectedListener(item -> {
+        
+        if (item.getItemId() == R.id.navCerrarSesion) {
+
+        Intent intent = new Intent(
+                PronosticosActivity.this,
+                MainActivity.class
+        );
+
+        intent.setFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK
+        );
+
+        startActivity(intent);
+        return true;
+    }
+
+    return false;
+});
+
+btVolver.setOnClickListener(v -> {
+    finish();
+});
     }
 }
